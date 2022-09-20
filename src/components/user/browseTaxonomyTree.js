bec69ca000f5f8da9f68e57f90967bd13cc32812
Matt <mattmartino54@gmail.com>
2022-09-20 14:43:58 -0500

    Added a filter to attach commonNames and the amphibian database primary key (to redirect to their specific taxa page with great info) to the animal added via browse taxonomy page.  Adds an entry into the Life List with the correct common names and aweb id

--- src/components/user/browseTaxonomyTree.js
+++ src/components/user/browseTaxonomyTree.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import "./profile.css"
 
 export const HerpTaxonomyList = () => {
 
     const localLiferUser = localStorage.getItem("lifer_user")
     const liferUserObject = JSON.parse(localLiferUser)
+    const navigate = useNavigate()
 
     const [reptiles, getReptiles] = useState([])
     const [amphibians, getAmphibians] = useState([])
@@ -29,7 +31,8 @@ export const HerpTaxonomyList = () => {
         subspecies: "",
         commonName: "",
         locationFound: "",
-        dateFound: ""
+        dateFound: "",
+        aweb_uid: ""
     })
 
 
@@ -171,8 +174,9 @@ export const HerpTaxonomyList = () => {
 
                 const uniqueReptiles = new Set(filteredByReptiles)
                 const uniqueReptileArray = Array.from(uniqueReptiles)
-
                 setFilterSubspecies(uniqueReptileArray)
+
+
             }
             else {
                 const filteredByAmphibians = amphibians.reduce((arr, herp) => {
@@ -186,6 +190,7 @@ export const HerpTaxonomyList = () => {
                 const uniqueAmphibians = new Set(filteredByAmphibians)
                 const uniqueAmphibianArray = Array.from(uniqueAmphibians)
                 setFilterSubspecies(uniqueAmphibianArray)
+
             }
 
         },
@@ -194,131 +199,191 @@ export const HerpTaxonomyList = () => {
 
 
 
+    const foundItClick = (event) => {
+        event.preventDefault()
+        
+        if (parseInt(newLifer.herpTypeId) > 5) {
+
+            if (newLifer.subspecies !== "") {
+                const filterSpecificAmphib = amphibians.filter(herp => herp.subspecies === newLifer.subspecies && herp.genus === newLifer.genus && herp.species === newLifer.species)
+                filterSpecificAmphib.map(herp => {
+
+                    newLifer.commonName = herp.commonName
+                    newLifer.aweb_uid = herp.aweb_uid 
+
+                })
+            }
+            else {
+                const filterSpecificAmphib = amphibians.filter(herp => herp.species === newLifer.species && herp.genus === newLifer.genus && herp.family === newLifer.family)
+                filterSpecificAmphib.map(herp => {
+                    debugger
+                    newLifer.commonName = herp.commonName
+                    newLifer.aweb_uid = herp.aweb_uid 
+
+            })
+        }
+    }
+
+
+    const newHerpToSendToAPI = {
+        usersId: liferUserObject.id,
+        herpTypeId: parseInt(newLifer.herpTypeId),
+        order: newLifer.order,
+        family: newLifer.family,
+        genus: newLifer.genus,
+        species: newLifer.species,
+        subspecies: newLifer.subspecies,
+        commonName: newLifer.commonName,
+        locationFound: newLifer.locationFound,
+        dateFound: newLifer.dateFound,
+        aweb_uid: newLifer.aweb_uid
+    }
+
+
+    return fetch(`http://localhost:8088/lifers`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newHerpToSendToAPI)
+    })
+        .then(response => response.json())
+        .then(() => {
+            navigate("/profile")
+        })
+
+}
 
 
 
 
-    return <>
-        <article className="HerpTypes" >
-            <form className="HerpType">
-                <fieldset className="orderBoxMain">
-                    <label htmlFor="name" className="orderBox">Select type of herp to browse</label>
-                    {
-                        sortedOrders.map((order) => {
-                            return <article className="orderType" key={`type--${order.id}`}>
-                                <div className="order" >
-                                    <input type="radio" name="OnlyOne"
+return <>
+    <article className="HerpTypes" >
+        <form className="HerpType">
+            <fieldset className="orderBoxMain">
+                <label htmlFor="name" className="orderBox">Select type of herp to browse</label>
+                {
+                    sortedOrders.map((order) => {
+                        return <article className="orderType" key={`type--${order.id}`}>
+                            <div className="order" >
+                                <input type="radio" name="OnlyOne"
 
-                                        onChange={
-                                            (evt) => {
-                                                const copy = { ...newLifer }
-                                                copy.herpTypeId = evt.target.value
-                                                copy.order = order.order
-                                                copy.genus = ""
-                                                copy.species = ""
-                                                copy.subspecies = ""
-                                                updateNewLifer(copy)
-                                                triggerFamilySelector(Date.now)
-
-
-                                            }
-                                        } value={order.id} /> {order.herpType}
-
-                                </div>
-                            </article>
-                        })
-                    }
-                </fieldset>
-
-                <fieldset>
-                    <div className="form-group" >
-                        <label htmlFor="description">Family:</label>
-                        <select id="descriptionFamily" value={newLifer.family}
-                            onChange={(evt) => {
-                                const copy = { ...newLifer }
-                                copy.family = evt.target.value
-                                updateNewLifer(copy)
-                                filterGenusTrigger(Date.now)
-                            }}
-                        >
-                            <option value={0}>Select the family you'd like to view</option>
-                            {
-                                filteredFamily.map(herp => {
-                                    return <option key={`family--${herp.id}`} value={herp.family}>{herp.family}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
-
-                <fieldset>
-                    <div className="form-group" >
-                        <label htmlFor="description2">Genus:</label>
-                        <select id="descriptionGenus" value={newLifer.genus}
-                            onChange={(evt) => {
-                                const copy = { ...newLifer }
-                                copy.genus = evt.target.value
-                                updateNewLifer(copy)
-                                filterSpeciesTrigger(Date.now)
-                            }}
-                        >
-                            <option value={0}>Select the Genus you'd like to view</option>
-                            {
-                                filteredGenus.map(herp => {
-                                    return <option key={`genus--${herp}`} value={herp}>{herp}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
-
-                <fieldset>
-                    <div className="form-group" >
-                        <label htmlFor="description">Species:</label>
-                        <select id="description" value={newLifer.species}
-                            onChange={(evt) => {
-                                const copy = { ...newLifer }
-                                copy.species = evt.target.value
-                                updateNewLifer(copy)
-                                filterSubSpeciesTrigger(Date.now)
-                            }}
-                        >
-                            <option value={0}>Select the Species you'd like to view</option>
-                            {
-                                filteredSpecies.map(herp => {
-                                    return <option key={`species--${herp}`} value={herp}>{herp}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
-
-                <fieldset>
-                    <div className="form-group" >
-                        <label htmlFor="description">SubSpecies:</label>
-                        <select id="description" value={newLifer.subspecies}
-                            onChange={(evt) => {
-                                const copy = { ...newLifer }
-                                copy.subspecies = evt.target.value
-                                updateNewLifer(copy)
-                            }}
-                        >
-                            <option value={0}>Some animals do not have a subspecies</option>
-                            {
-                                filteredSubspecies.map(herp => {
-                                    return <option key={`species--${herp}`} value={herp}>{herp}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
+                                    onChange={
+                                        (evt) => {
+                                            const copy = { ...newLifer }
+                                            copy.herpTypeId = evt.target.value
+                                            copy.order = order.order
+                                            copy.genus = ""
+                                            copy.species = ""
+                                            copy.subspecies = ""
+                                            updateNewLifer(copy)
+                                            triggerFamilySelector(Date.now)
 
 
+                                        }
+                                    } value={order.id} /> {order.herpType}
+
+                            </div>
+                        </article>
+                    })
+                }
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group" >
+                    <label htmlFor="description">Family:</label>
+                    <select id="descriptionFamily" value={newLifer.family}
+                        onChange={(evt) => {
+                            const copy = { ...newLifer }
+                            copy.family = evt.target.value
+                            updateNewLifer(copy)
+                            filterGenusTrigger(Date.now)
+                        }}
+                    >
+                        <option value={0}>Select the family you'd like to view</option>
+                        {
+                            filteredFamily.map(herp => {
+                                return <option key={`family--${herp.id}`} value={herp.family}>{herp.family}</option>
+                            })
+                        }
+                    </select>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group" >
+                    <label htmlFor="description2">Genus:</label>
+                    <select id="descriptionGenus" value={newLifer.genus}
+                        onChange={(evt) => {
+                            const copy = { ...newLifer }
+                            copy.genus = evt.target.value
+                            updateNewLifer(copy)
+                            filterSpeciesTrigger(Date.now)
+                        }}
+                    >
+                        <option value={0}>Select the Genus you'd like to view</option>
+                        {
+                            filteredGenus.map(herp => {
+                                return <option key={`genus--${herp}`} value={herp}>{herp}</option>
+                            })
+                        }
+                    </select>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group" >
+                    <label htmlFor="description">Species:</label>
+                    <select id="description" value={newLifer.species}
+                        onChange={(evt) => {
+                            const copy = { ...newLifer }
+                            copy.species = evt.target.value
+                            updateNewLifer(copy)
+                            filterSubSpeciesTrigger(Date.now)
+                        }}
+                    >
+                        <option value={""}>Select the Species you'd like to view</option>
+                        {
+                            filteredSpecies.map(herp => {
+                                return <option key={`species--${herp}`} value={herp}>{herp}</option>
+                            })
+                        }
+                    </select>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group" >
+                    <label htmlFor="description">SubSpecies:</label>
+                    <select id="description" value={newLifer.subspecies}
+                        onChange={(evt) => {
+                            const copy = { ...newLifer }
+                            copy.subspecies = evt.target.value
+                            updateNewLifer(copy)
+                        }}
+                    >
+                        <option value={0}>Some animals do not have a subspecies</option>
+                        {
+                            filteredSubspecies.map(herp => {
+                                
+                                if (herp !== undefined )
+                                return <option key={`species--${herp}`} value={herp}>{herp}</option>
+                            })
+                        }
+                    </select>
+                </div>
+            </fieldset>
 
 
-            </form>
+            <button
+                onClick={(clickEvent) => foundItClick(clickEvent)}
+                className="btn btn-primary">
+                Found it before!
+            </button>
 
-        </article>
+        </form>
 
-    </>
+    </article>
+
+</>
 }
