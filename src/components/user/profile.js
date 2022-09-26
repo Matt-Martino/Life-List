d7e98547acc253b2dd708e4b6c26261eb8895372
Matt <mattmartino54@gmail.com>
2022-09-26 14:29:03 -0500

    Added an upload picture option in the profile section.  Styled the upload image page as well.

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
 import "./profile.css"
 
-export const YourLifeList = ({searchTermState}) => {
+export const YourLifeList = ({ searchTermState }) => {
 
     const [lifeList, setLifeList] = useState([])
     const [filteredLifeList, setFiltered] = useState([])
@@ -11,16 +12,18 @@ export const YourLifeList = ({searchTermState}) => {
     const localLiferUser = localStorage.getItem("lifer_user")
     const liferUserObject = JSON.parse(localLiferUser)
 
+
+
     useEffect(
         () => {
             const searchedProfile = lifeList.filter(herp => {
                 return herp.order.toLowerCase().includes(searchTermState.toLowerCase())
-                || herp.family.toLowerCase().includes(searchTermState.toLowerCase())
-                || herp.genus.toLowerCase().includes(searchTermState.toLowerCase())
-                || herp.species.toLowerCase().includes(searchTermState.toLowerCase())
-                || herp.commonName.toLowerCase().includes(searchTermState.toLowerCase())
-                                
-                })
+                    || herp.family.toLowerCase().includes(searchTermState.toLowerCase())
+                    || herp.genus.toLowerCase().includes(searchTermState.toLowerCase())
+                    || herp.species.toLowerCase().includes(searchTermState.toLowerCase())
+                    || herp.commonName.toLowerCase().includes(searchTermState.toLowerCase())
+
+            })
             setFiltered(searchedProfile)
         },
         [searchTermState]
@@ -61,70 +64,84 @@ export const YourLifeList = ({searchTermState}) => {
 
 
 
-    return <>
-        <section className="mainbox">
-            <h2 className="Header-text-edit">Your Life List!</h2>
-            <article className="HerpTypes">
 
-                {
-                    filteredLifeList.map(
-                        (list) => {
-                            return <section className="HerpType" key={`herp--${list.id}`}>
-                                <ul className="list">Order: {list.order}</ul>
-                                <ul className="list">Family:  {list.family}</ul>
-                                <ul className="list">Genus:  <i>{list.genus}</i></ul>
-                                <ul className="list">Species:  <i>{list.species}</i></ul>
-                                {
-                                    list.subspecies
-                                    ?<ul className="list">Subspecies:  <i>{list.subspecies}</i></ul>
-                                    :""
-                                }
-                                {
-                                    list.commonName
-                                    ?<ul className="list">Common Name:  {list.commonName}</ul>
-                                    :""
-                                }
-                                {
-                                    list.locationFound
-                                    ?<ul className="list">Location Found:  {list.locationFound}</ul>
-                                    :""
-                                }
-                                {
-                                    list.dateFound
-                                    ?<ul className="list">Date Found:  {list.dateFound}</ul>
-                                    :""
-                                }
-                                {
-                                    list.aweb_uid
+return <>
+    <section className="mainbox">
+        <h2 className="Header-text-edit">Your Life List!</h2>
+        <article className="HerpTypes">
+
+            {
+                filteredLifeList.map(
+                    (list) => {
+                        return <section className="HerpType" key={`herp--${list.id}`}>
+                            <ul className="list">Order: {list.order}</ul>
+                            <ul className="list">Family:  {list.family}</ul>
+                            <ul className="list">Genus:  <i>{list.genus}</i></ul>
+                            <ul className="list">Species:  <i>{list.species}</i></ul>
+                            {
+                                list.subspecies
+                                    ? <ul className="list">Subspecies:  <i>{list.subspecies}</i></ul>
+                                    : ""
+                            }
+                            {
+                                list.commonName
+                                    ? <ul className="list">Common Name:  {list.commonName}</ul>
+                                    : ""
+                            }
+                            {
+                                list.locationFound
+                                    ? <ul className="list">Location Found:  {list.locationFound}</ul>
+                                    : ""
+                            }
+                            {
+                                list.dateFound
+                                    ? <ul className="list">Date Found:  {list.dateFound}</ul>
+                                    : ""
+                            }
+                            {
+                                list.aweb_uid
                                     ? <ul className="list">Life history data:  <a href={`https://amphibiaweb.org/species/${list.aweb_uid}`} target="_blank" >
-                                    View {list.genus} {list.species} data.
+                                        View {list.genus} {list.species} data.
                                     </a>
                                     </ul>
                                     : ""
 
-                                }
+                            }
+                            {
+                                list.image
+                                ? <img src={list.image} className="uploaded_image"
+                                ></img>
+                                :""
+                            }
 
-                                <button onClick={
-                                    () => navigate(`${list.id}/edit`)}
-                                    className="btn btn-primary">
-                                    Edit</button>
+                            <button onClick={
+                                () => navigate(`${list.id}/edit`)}
+                                className="btn btn-primary">
+                                Edit</button>
 
-                                <button onClick={() => {
-                                    fetch(`http://localhost:8088/lifers/${list.id}`, {
-                                        method: "DELETE"
+                            <button onClick={() => {
+                                fetch(`http://localhost:8088/lifers/${list.id}`, {
+                                    method: "DELETE"
+                                })
+                                    .then(() => {
+                                        setDeleteChange(Date.now)
                                     })
-                                        .then(() => {
-                                            setDeleteChange(Date.now)
-                                        })
-                                }} className="btn btn-primary"
+                            }} className="btn btn-primary"
 
-                                >Delete</button>
-                            </section>
-                        }
-                    )
-                }
+                            >Delete</button>
 
-            </article>
-        </section>
-    </>
+                            <button onClick={
+                                () => navigate(`${list.id}/editImage`)}
+                                className="btn btn-primary">
+                                Add image</button>
+
+
+                        </section>
+                    }
+                )
+            }
+
+        </article>
+    </section>
+</>
 }
